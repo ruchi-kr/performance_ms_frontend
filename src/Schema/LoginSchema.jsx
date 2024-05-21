5b290e05ebc918f9726e40eec77a26be88c45d8f
ruchi <ruchi.kumari63@gmail.com>
2024-05-21 12:25:19 +0530

    changes made in login changed to email id

--- src/Schema/LoginSchema.jsx
+++ src/Schema/LoginSchema.jsx
@@ -1,10 +1,10 @@
 import * as Yup from 'yup';
 
 const LoginSchema = Yup.object().shape({
-  username: Yup.string()
-    .min(4, 'Username must be at least 4 characters')
-    .max(30, 'Username must be less than 20 characters')
-    .required('Username is required'),
+  email_id: Yup.string()
+    // .min(4, 'Username must be at least 4 characters')
+    // .max(30, 'Username must be less than 20 characters')
+    .required('Email is required'),
   password: Yup.string()
     .min(8, 'Password must be at least 8 characters')
     .max(20, 'Password must be less than 20 characters')
